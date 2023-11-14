f8fb53b70ee5ba6a7bb1e3158080cdd57f341428
Aksa <aksa@progate.com>
2023-11-14 14:14:05 +0700

    Apply common layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TaskController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,8 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
 
-    return view('home'); // Diperbarui
+    return view('home');
 
-});
+})->name('home'); // name ditambahkan
+
+Route::get('/tasks/', [TaskController::class, 'index'])->name('tasks.index');
