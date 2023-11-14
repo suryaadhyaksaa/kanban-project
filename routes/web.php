16d17b6997dc3f57931b784785160223349e6b27
Aksa <aksa@progate.com>
2023-11-14 13:16:39 +0700

    Develop TaskList View

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('home');
+});
 
-    return view('home'); // Diperbarui
+
+use App\Http\Controllers\TaskController; // Ditambahkan
+
+Route::get('/', function () {
+
+    return view('home');
 
 });
+
+
+Route::get('/tasks/', [TaskController::class, 'index']); // Ditambahka
+
